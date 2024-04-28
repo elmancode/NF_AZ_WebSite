d5fcb4ed2eb12593c89acada0a8b661e61534d40
Şəhriyar Məmmədov <76937208+Sehriyar4473@users.noreply.github.com>
2024-04-28 19:29:41 +0400

    section1

--- src/layouts/header/index.jsx
+++ src/layouts/header/index.jsx
@@ -1,26 +1,141 @@
-import React from 'react'
-import Logo from "../../assets/logo white.png"
-import "./index.scss"
+import React, { useEffect, useRef, useState } from "react";
+import Logo from "../../assets/logo white.png";
+import hamburger_menu from "../../assets/icons/hamburger_menu.png";
+import facebook from "../../assets/icons/facebook.png";
+import twitter from "../../assets/icons/twitter.png";
+import behance from "../../assets/icons/behance.png";
+import instagram from "../../assets/icons/instagram.png";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import "./index.scss";
 
 const Header = () => {
+  const [colorChange, setColorChange] = useState(false);
+  const [open, setOpen] = useState(false);
+  const drawerRef = useRef(null);
+
+  const scrollThreshold = 150;
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+
+      if (currentScrollPos >= scrollThreshold) {
+        setColorChange(true);
+      } else {
+        setColorChange(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+
+    AOS.init({
+      duration: 1200,
+    });
+
+    window.addEventListener("resize", () => {
+      AOS.refresh();
+    });
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (open) {
+      AOS.refresh();
+    }
+  }, [open]);
+
+  const handleClickOutside = (event) => {
+    if (drawerRef.current && !drawerRef.current.contains(event.target)) {
+      setOpen(false);
+    }
+  };
+
   return (
-    <header>
+    <>
+      <header>
+        <div className="left" data-aos="fade-down">
+          <img src={Logo} alt="logo" />
+        </div>
 
-      <div className="left">
-        <img src={Logo} alt="logo" />
+        <div
+          className={colorChange ? "right colorChange" : "right"}
+          onClick={() => setOpen(true)}
+        >
+          <p> MENU </p>
+          <img src={hamburger_menu} alt="menu" />
+        </div>
+      </header>
+
+      <div id="drawer" className={open ? "open" : "closed"} ref={drawerRef}>
+        <div className={open ? "drawerContent  open" : "drawerContent closed"}>
+          <div className="head">
+            <h4>NAVIGATION</h4>
+            <div className="closeIcon" onClick={() => setOpen(false)}>
+              x
+            </div>
+          </div>
+
+          <nav>
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Contact</a>
+            <a href="#">Works</a>
+            <a href="#">Clients</a>
+          </nav>
+
+          <p>
+            Perspiciatis hic praesentium nesciunt. Et neque a dolorum voluptatem
+            porro iusto sequi veritatis libero enim. Iusto id suscipit veritatis
+            neque reprehenderit.
+          </p>
+
+          <div className="socialMediaIcons">
+            <a
+              href="http://facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={facebook} alt="facebook" />
+            </a>
+
+            <a
+              href="http://facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={instagram} alt="facebook" />
+            </a>
+
+            <a
+              href="http://facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={twitter} alt="facebook" />
+            </a>
+
+            <a
+              href="http://facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={behance} alt="facebook" />
+            </a>
+          </div>
+        </div>
       </div>
+    </>
+  );
+};
 
-      <div className="right">
-        <p>  MENU  </p>
-        <span> = </span>
-         
-        
-      
-      </div>
-
-
-    </header>
-  )
-}
-
-export default Header
+export default Header;
--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -1,14 +1,87 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./index.scss";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+import facebook from "../../assets/icons/facebook.png";
+import twitter from "../../assets/icons/twitter.png";
+import behance from "../../assets/icons/behance.png";
+import instagram from "../../assets/icons/instagram.png";
 
 const HomePage = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1200,
+    });
+
+    window.addEventListener("resize", () => {
+      AOS.refresh();
+    });
+
+    return () => {
+      window.removeEventListener("resize", () => {
+        AOS.refresh();
+      });
+    };
+  }, []);
+
   return (
-    <div>
+    <div id="homePage">
       <section id="section1">
         <div className="overlay"> </div>
 
+        <div className="container" data-aos="fade-up">
+          <b>Təbiət Dostlarına xoş gəlmisiniz</b>
+          <h1>
+            TƏBİƏT DOSTLARI DÜNYANIN YÜZLƏRLƏ ÖLKƏSİNDƏ ƏTRAF MÜHİTİN
+            MÜHAFİZƏSİNDƏ TÖHVƏ VERMƏK MƏQSƏDİ İLƏ FƏALİYYƏTDƏDİR.  
+          </h1>
+        </div>
+
+        <div className="socialMediaIcons">
+          <a
+            href="http://facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={facebook} alt="facebook" />
+            <p>facebook</p>
+          </a>
+
+          <a
+            href="http://facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={instagram} alt="facebook" />
+            <p>twitter</p>
+          </a>
+
+          <a
+            href="http://facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={twitter} alt="facebook" />
+            <p>instagram</p>
+          </a>
+
+          <a
+            href="http://facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={behance} alt="facebook" />
+            <p>behance</p>
+          </a>
+        </div>
+      </section>
+
+      <section id="section2">
+        <div className="overlay"> </div>
+
         <div className="container">
-          <b>Təbiət Dostlarına xoş gəlmisiz</b>
+          <b>Təbiət Dostlarına xoş gəlmisiniz</b>
           <h1>
             TƏBİƏT DOSTLARI DÜNYANIN YÜZLƏRLƏ ÖLKƏSİNDƏ ƏTRAF MÜHİTİN
             MÜHAFİZƏSİNDƏ TÖHVƏ VERMƏK MƏQSƏDİ İLƏ FƏALİYYƏTDƏDİR.  
