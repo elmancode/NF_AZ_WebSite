5f7d43d05ebfd71ee79793463bb0701b1af5c4f1
Şəhriyar Məmmədov <76937208+Sehriyar4473@users.noreply.github.com>
2024-05-02 10:32:37 +0400

    section2 section3

---
+++ src/companents/contactUS/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./index.scss";
+
+const ContactUS = () => {
+  return <div>ContactUS</div>;
+};
+
+export default ContactUS;
--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -1,11 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import "./index.scss";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { FaFacebookF } from "react-icons/fa";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaLinkedinIn,
+  FaBullhorn,
+  FaBezierCurve,
+  FaBrush,
+  FaGlobe,
+} from "react-icons/fa";
 import { BsInstagram } from "react-icons/bs";
-import { FaLinkedinIn } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import useScrollTriggeredCountUp from "../../companents/ScrollTriggetCountUp";
 
 const HomePage = () => {
   useEffect(() => {
@@ -24,6 +32,12 @@ const HomePage = () => {
     };
   }, []);
 
+  const ref = useRef();
+  const count1 = useScrollTriggeredCountUp(ref, 100);
+  const count2 = useScrollTriggeredCountUp(ref, 150);
+  const count3 = useScrollTriggeredCountUp(ref, 1450);
+  const count4 = useScrollTriggeredCountUp(ref, 5000);
+
   return (
     <div id="homePage">
       <section id="section1">
@@ -35,9 +49,14 @@ const HomePage = () => {
             Təbiət Dostları dünyanın yüzlərlə ölkəsində ətraf mühitin
             mühafizəsində töhvə vermək məqsədi ilə fəaliyyətdədir. 
           </h1>
+
+          <div className="buttons">
+            <Link to={"/"}>START A PROJECT</Link>
+            <Link to={"/"}>MORE ABOUT US</Link>
+          </div>
         </div>
 
-        <div className="socialMediaIcons">
+        <div className="socialMediaIcons" data-aos="zoom-in-left">
           <a
             href="https://www.facebook.com/NatureFriends"
             target="_blank"
@@ -74,17 +93,132 @@ const HomePage = () => {
             <p>Twitter</p>
           </a>
         </div>
+        <h1>salam</h1>
       </section>
 
       <section id="section2">
         <div className="overlay"> </div>
 
         <div className="container">
-          <b>Təbiət Dostlarına xoş gəlmisiniz</b>
-          <h1>
-            TƏBİƏT DOSTLARI DÜNYANIN YÜZLƏRLƏ ÖLKƏSİNDƏ ƏTRAF MÜHİTİN
-            MÜHAFİZƏSİNDƏ TÖHVƏ VERMƏK MƏQSƏDİ İLƏ FƏALİYYƏTDƏDİR.  
-          </h1>
+          <p>HELLO THERE</p>
+          <h1>We Are Glint</h1>
+
+          <p className="description">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt.
+          </p>
+
+          <div className="counts" data-aos="fade-up">
+            <div className="count">
+              <h2 ref={ref}>{count1}</h2>
+              <h4>Awards Received</h4>
+            </div>
+            <hr />
+
+            <div className="count">
+              <h2 ref={ref}>{count2}</h2>
+              <h4>Awards Received</h4>
+            </div>
+            <hr />
+
+            <div className="count">
+              <h2 ref={ref}>{count3}</h2>
+              <h4>Awards Received</h4>
+            </div>
+            <hr />
+
+            <div className="count">
+              <h2 ref={ref}>{count4}</h2>
+              <h4>Awards Received</h4>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="section3">
+        <div className="container">
+          <div className="head">
+            <h2>WHAT WE DO</h2>
+
+            <h1>
+              We’ve got everything you need to launch and grow your business
+            </h1>
+          </div>
+
+          <div className="cards">
+            <div className="card">
+              <div className="">
+                <FaBrush className="icon" />
+              </div>
+
+              <div className="cardHead">
+                <h2>Brand Identity</h2>
+
+                <p className="text">
+                  Nemo cupiditate ab quibusdam quaerat impedit magni. Earum
+                  suscipit ipsum laudantium. Quo delectus est. Maiores voluptas
+                  ab sit natus veritatis ut. Debitis nulla cumque veritatis.
+                  Sunt suscipit voluptas ipsa in tempora esse soluta sint.
+                </p>
+              </div>
+            </div>
+
+            <div className="card">
+              <div className="">
+                <FaBezierCurve className="icon" />
+              </div>
+
+              <div className="cardHead">
+                <h2>Illustration</h2>
+
+                <p className="text">
+                  Nemo cupiditate ab quibusdam quaerat impedit magni. Earum
+                  suscipit ipsum laudantium. Quo delectus est. Maiores voluptas
+                  ab sit natus veritatis ut. Debitis nulla cumque veritatis.
+                  Sunt suscipit voluptas ipsa in tempora esse soluta sint.
+                </p>
+              </div>
+            </div>
+
+            <div className="card">
+              <div className="">
+                <FaBullhorn className="icon" />
+              </div>
+
+              <div className="cardHead">
+                <h2>Marketing</h2>
+
+                <p className="text">
+                  Nemo cupiditate ab quibusdam quaerat impedit magni. Earum
+                  suscipit ipsum laudantium. Quo delectus est. Maiores voluptas
+                  ab sit natus veritatis ut. Debitis nulla cumque veritatis.
+                  Sunt suscipit voluptas ipsa in tempora esse soluta sint.
+                </p>
+              </div>
+            </div>
+
+            <div className="card">
+              <div className="">
+                <FaGlobe className="icon" />
+              </div>
+
+              <div className="cardHead">
+                <h2>Web Design</h2>
+
+                <p className="text">
+                  Nemo cupiditate ab quibusdam quaerat impedit magni. Earum
+                  suscipit ipsum laudantium. Quo delectus est. Maiores voluptas
+                  ab sit natus veritatis ut. Debitis nulla cumque veritatis.
+                  Sunt suscipit voluptas ipsa in tempora esse soluta sint.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </div>
